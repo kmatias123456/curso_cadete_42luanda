234a94750526f155dee22dd7dfd858c29891b6e5
kmatias123456 <kiangebenimatias4@gmail.com>
2024-05-18 19:11:32 +0100

    tes

--- strtrim/strtrim.c
+++ strtrim/strtrim.c
@@ -6,16 +6,33 @@ static int is_set_char(char c, char const *set)
     while (*set)
     {
         if (c == *set)
-            return (1);
+            return 1;
         set++;
     }
-    return (0);
+    return 0;
+}
+
+void ft_start(char const *s1, char const *set, int *start)
+{
+    while (s1[*start] && is_set_char(s1[*start], set))
+        (*start)++;
+}
+
+void ft_end(char const *s1, char const *set, int *len, int start)
+{
+    int end = start;
+    *len = start;
+    while (s1[end])
+    {
+        if (!is_set_char(s1[end], set))
+            *len = end + 1;
+        end++;
+    }
 }
 
 char *ft_strtrim(char const *s1, char const *set)
 {
     int start;
-    int end;
     int len;
     int i;
     char *trimmed;
@@ -23,16 +40,9 @@ char *ft_strtrim(char const *s1, char const *set)
     if (!s1 || !set)
         return NULL;
     start = 0;
-    while (s1[start] && is_set_char(s1[start], set))
-        start++;
-    end = start;
+    ft_start(s1, set, &start);
     len = 0;
-    while (s1[end])
-    {
-        if (!is_set_char(s1[end], set))
-            len = end + 1;
-        end++;
-    }
+    ft_end(s1, set, &len, start);
     trimmed = (char *)malloc((len - start + 1) * sizeof(char));
     if (!trimmed)
         return NULL;
